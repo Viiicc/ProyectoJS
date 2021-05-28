c350d6151545a98e505afeb407cc5378c6b27071
Viiicc <victor.lieggi@gmail.com>
2021-05-27 23:46:48 -0300

    Proyecto JS

---
+++ JS/data.js
@@ -0,0 +1,65 @@
+var productos= [
+  {
+    "id": 1,
+     "nombre": "Sanguche de Mila Completo",
+     "precio": 770,
+     "destacado": true,
+     "imagen": "./IMAGES/Sanguche1.png",
+  },
+  {
+    "id": 2,
+     "nombre": "Hamburguesa Completa",
+     "precio": 550,
+     "destacado": true,
+     "imagen": "./IMAGES/Burger.png"
+  },
+  {
+    "id": 3,
+     "nombre": "Porcion de Papas Fritas",
+     "precio": 400,
+     "destacado": true,
+     "imagen": "./IMAGES/papasFritas.png"
+  },
+  {
+    "id": 4,
+     "nombre": "Bife de Chorizo",
+     "precio": 890,
+     "destacado": true,
+     "imagen": "./IMAGES/bife.png"
+  },
+  {
+    "id": 5,
+     "nombre": "Pollo Asado",
+     "precio": 600,
+     "destacado": true,
+     "imagen": "./IMAGES/pollo.png"
+  },
+  {
+    "id": 6,
+     "nombre": "Ensalada Mixta",
+     "precio": 280,
+     "destacado": true,
+     "imagen": "./IMAGES/salad.png"
+  },
+  {
+    "id": 7,
+     "nombre": "Coca-Cola",
+     "precio": 100,
+     "destacado": true,
+     "imagen": "./IMAGES/CocaCola.png"
+  },
+  {
+    "id": 8,
+     "nombre": "Cerveza Heineken 470cc",
+     "precio": 120,
+     "destacado": true,
+     "imagen": "./IMAGES/Cerveza.png"
+  },
+  {
+    "id": 9,
+     "nombre": "Sushi Mixto (15 piezas)",
+     "precio": 700,
+     "destacado": false,
+     "imagen": "./IMAGES/sushi.png"
+  },
+]
